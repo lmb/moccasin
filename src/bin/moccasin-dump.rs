4bf25bf32ef147ae8e1958d8a662ca2c34fceaf0
lmb <i@lmb.io>
2015-10-15 12:02:57 +0100

    Make moccasin-dump dump hex encoded arguments

--- src/bin/moccasin-dump.rs
+++ src/bin/moccasin-dump.rs
@@ -1,46 +1,25 @@
 extern crate moccasin;
+extern crate rustc_serialize;
 
-use std::fs::File;
-use std::io::prelude::*;
+use std::env;
 
-use moccasin::{Parser, Token};
-use moccasin::types::{String, FromToken};
-use moccasin::Tag::PrintableString;
+use rustc_serialize::hex::FromHex;
+use moccasin::prelude::*;
 
 // For some reason, cargo test builds this executable and complains about unused
 // code.
 #[allow(dead_code)]
 fn main() {
-	let mut data: Vec<u8> = Vec::new();
+	let mut args = env::args();
+	args.next().unwrap();
 
-	let mut file = match File::open("tests/cert.der") {
-		Err(why) => panic!("Can't open file: {:?}", why),
-		Ok(file) => file,
-	};
+	for arg in args {
+		let bytes = arg.from_hex().unwrap();
 
-	if let Err(why) = file.read_to_end(&mut data) {
-		panic!("Can't read file: {:?}", why)
-	}
+		let p = Parser::new(&bytes);
 
-	let p = Parser::new(&data);
-
-	for token in p {
-		match token {
-			Ok(ref t @ Token{tag: PrintableString, ..}) => {
-				let s = if let Ok(String(string)) = String::from_token(&t) {
-					string
-				} else {
-					"INVALID"
-				};
-
-				let &Token{ref tag, ref enc, ref body, ref depth, ..} = t;
-
-				println!("d={: >2} {:?}, {:?}, {: >3}: {}", depth, enc, tag, body.len(), s);
-			},
-			Ok(Token{enc, tag, body, depth, ..}) => {
-				println!("d={: >2} {:?}, {:?}, {: >3}", depth, enc, tag, body.len())
-			},
-			Err(why) => panic!("Parse error: {:?}", why)
+		for t in p {
+			println!("{:?}", t);
 		}
 	}
 }
